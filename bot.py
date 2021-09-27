78885b8f08c5d68d9db8f7add8cc7660c3c82c1c
lopar6 <lopar6@gmail.com>
2021-09-27 13:24:35 -0600

    added channel recognition

--- bot.py
+++ bot.py
@@ -1,16 +1,43 @@
+from gc import enable
 import os
+import asyncio
+from typing import Tuple
 
 import discord
+from discord import message
+from discord import channel
+from discord.flags import Intents
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# print(TOKEN)
-client = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+myChannel = None
 
 @client.event
 async def on_ready():
+    await client.wait_until_ready()
     print(f'{client.user} has connected to Discord!')
+    # ! add channel id number here
+    # make sure to enable dev mode on discord
+    myChannel = client.get_channel(888455189609201727)
+    if myChannel is None:
+        print("ERROR: channel not found")
+    else:
+        await myChannel.send("hello")
+
+# todo fix this to see all new members
+@client.event
+async def on_member_join(member):
+    await myChannel.send("who tf?")
+
+
+# todo add tuples of options for the bot to say
+# todo add random selection each time member joins, then send
+
 
 client.run(TOKEN)
