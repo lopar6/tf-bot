4d8fbe064c3cdd4d1fcf23d5f9d08140ed107505
lopar6 <lopar6@gmail.com>
2021-09-27 14:05:17 -0600

    added meat and potatoes

--- bot.py
+++ bot.py
@@ -1,13 +1,21 @@
 from gc import enable
 import os
-import asyncio
+import random
 from typing import Tuple
+
 
 import discord
 from discord import message
 from discord import channel
 from discord.flags import Intents
 from dotenv import load_dotenv
+
+message_options = (
+    "Whom the fuk?",
+    "New member, who TF is this",
+    "Halt. Who tf goes there?",
+    "Who to heck?"
+)
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -16,7 +24,7 @@ intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 
-myChannel = None
+myChannel = client.get_channel(888455189609201727)
 
 @client.event
 async def on_ready():
@@ -24,20 +32,21 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     # ! add channel id number here
     # make sure to enable dev mode on discord
+    global myChannel
     myChannel = client.get_channel(888455189609201727)
     if myChannel is None:
         print("ERROR: channel not found")
     else:
-        await myChannel.send("hello")
+        await myChannel.send("Who tf am I?")
 
-# todo fix this to see all new members
 @client.event
 async def on_member_join(member):
-    await myChannel.send("who tf?")
+    print(f'{member} joined')
+    await myChannel.send(getSnarkyMessage())
 
+def getSnarkyMessage() :
+    return random.choice(("Who tf?", random.choice(message_options)))
 
 # todo add tuples of options for the bot to say
 # todo add random selection each time member joins, then send
-
-
 client.run(TOKEN)
