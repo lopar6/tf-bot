d3da473b5ed6a44023418179b28a8f4ef6acc397
lopar6 <lopar6@gmail.com>
2021-09-30 10:33:30 -0600

    bugfix google search user names + small refactor

--- bot.py
+++ bot.py
@@ -40,6 +40,13 @@ EMOJI_ID_OPTIONS = (
     847959736238604324
 )
 
+_search_params = {
+    'q': 'test',
+    'num': 1,
+    'safe': 'medium',
+    'fileType': 'jpg'
+}
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
@@ -59,7 +66,6 @@ google = GoogleImagesSearch(GOOGLE_SEARCH_KEY, PROJECT_CX)
 async def on_ready():
     await client.wait_until_ready()
     print(f'{client.user} has connected to Discord!')
-    # ! add channel id number here
     # make sure to enable dev mode on discord
     global myChannel
     myChannel = client.get_channel(CHANNEL_ID)
@@ -75,12 +81,22 @@ async def on_member_join(member):
     print(f'{member} joined')
     await myChannel.send(getSnarkyMessage())
     if random.randint(1, 2) == 2:
-        google.search(search_params=_search_params, path_to_dir='images/', custom_image_name="who-tf")
-        with open("images/who-tf.jpg", 'rb') as imageFile:
-            file = discord.File(imageFile)
-            await myChannel.send('Is this you?', file=file)
-        if os.path.exists('images/who-tf.jpg'):
-            os.remove('images/who-tf.jpg')
+        sendGoogledImageMessage(member)
+
+# call google api to find image with users name and send it to chat
+async def sendGoogledImageMessage(member):
+    _search_params = {
+    'q': member.name,
+    'num': 1,
+    'safe': 'medium',
+    'fileType': 'jpg'
+}
+    google.search(search_params=_search_params, path_to_dir='images/', custom_image_name="who-tf")
+    with open("images/who-tf.jpg", 'rb') as imageFile:
+        file = discord.File(imageFile)
+        await myChannel.send('Is this you?', file=file)
+    if os.path.exists('images/who-tf.jpg'):
+        os.remove('images/who-tf.jpg')
 
 def getSnarkyMessage() :
     # add check for time and asking why someone joined so late
@@ -88,19 +104,14 @@ def getSnarkyMessage() :
 
 # todo idea if a drawer organizer starts typing it interupts them with a .05% chance
 
-# todo replace q "test" with actual queue
-_search_params = {
-    'q': 'test',
-    'num': 1,
-    'safe': 'medium',
-    'fileType': 'jpg'
-}
-
 @client.event
 async def on_message(message):
     # if someone says whotf the bot reacts, occationally it replies
     # todo add check if self
     currentChannel = message.channel
+    if message.content.lower() == "hey tf bot. who am i?":
+        await sendGoogledImageMessage(message.author)
+
     if message.content.lower() in WHO_TF_OPTIONS:
         await message.add_reaction(client.get_emoji(random.choice(EMOJI_ID_OPTIONS)))
         if random.randint(0, 10) == 5:
